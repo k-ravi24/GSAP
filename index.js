ddf8c5c5edb9b42aa3768649625505eb5f6fc6d0
k-ravi24 <k.ravi19777@gmail.com>
2024-07-08 18:51:44 +0530

    added: timeline in index.js [GSAP]

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-gsap.from("#container", {
-  x: 1000,
-  y: 300,
-  duration: 3,
+// gsap.from("#container", {
+//   x: 1000,
+//   y: 300,
+//   duration: 3,
+//   delay: 2,
+//   rotate: 720,
+//   backgroundColor: "green",
+//   borderRadius: "50%",
+//   yoyo: true,
+//   repeat: 2,
+// });
+
+var timeline = gsap.timeline(); // timeline created
+
+timeline.to("#container", {
+  x: 1200,
+  y: 230,
+  color: "red",
+  rotate: 231,
   delay: 2,
-  rotate: 720,
-  backgroundColor: "green",
-  borderRadius: "50%",
-  yoyo: true,
-  repeat: 2,
+  duration: 3,
 });
